d43f3169a1cf4a91ba176aed3cf3b984436b20ac
AshrayMetaSoft <ashray@metasoftit.com>
2025-10-10 23:06:49 +0530

    updated the folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 
 import './App.css'
-import { ChevronLeft, ChevronRight, ShoppingCart, User } from 'lucide-react'
-import Header from './Header'
-import Banner from './Banner'
-import Band from './Band'
-import Tending from './TendingCollections'
+
+import Header from './components/common/Header'
+import Banner from './components/Home/Banner'
+import Band from './components/Home/Band'
+import Tending from './components/Home/TendingCollections'
 import Exclusive from './ExclusiveFashion'
-import Fashion from './Fashion'
-import Footer from './Footer'
+import Fashion from './components/Home/Fashion'
+import Footer from './components/common/Footer'
 
 function App() {
   return (
     <div className="bg-Brown text-white min-h-screen">
-    <Header/>
-    <main>
-    <Banner/>
-    <Band/>
-    <Tending/>
-    <Exclusive/>
-    <Fashion/>
-    </main>
-    <Footer/> 
+      <Header />
+      <main>
+        <Banner />
+        <Band />
+        <Tending />
+        <Exclusive />
+        <Fashion />
+      </main>
+      <Footer />
     </div>
   )
 }
--- src/components/Home/ExclusiveFashion.jsx
+++ src/components/Home/ExclusiveFashion.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import products from './data/Exclusive.js';
+import products from './utils/Exclusive.js';
 
 
 function ExclusiveFashion() {
